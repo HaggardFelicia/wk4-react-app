6473cafda2404617b75c071068422afe22e98196
HaggardFelicia <haggardfelicia@outlook.com>
2024-03-04 09:40:09 -0600

    updating files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './index.css';
 import SearchBar from './components/SearchBar';
+import API from './API'
 
 function App(){
   const handleSearch = async event => {
     event.preventDefault();
     console.log("searching...", event.target.search.value);
+    const response = await API.fetchImages(event.target.search.value);
+    console.log('response', response);
   };
 
   return(
